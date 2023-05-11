5e0c5cece0637b451120bb9d810e6f9842bc9028
cloverpig <byoungoh.kang@gmail.com>
2023-05-11 16:10:50 +0900

    음

--- src/main/java/com/greedy/moaware/employee/repository/DeptRepository.java
+++
@@ -1,9 +0,0 @@
-package com.greedy.moaware.employee.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.greedy.moaware.employee.entity.Dept;
-
-public interface DeptRepository extends JpaRepository<Dept, Integer>{
-
-}
