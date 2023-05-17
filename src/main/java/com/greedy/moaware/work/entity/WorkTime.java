753a35555acf496bf1b266751c722a189dd88e7a
dldbaud <dldbaud20@gmail.com>
2023-05-17 20:07:58 +0900

    유명 / test : 출근,퇴근 버튼 개발중

---
+++ src/main/java/com/greedy/moaware/work/entity/WorkTime.java
@@ -0,0 +1,31 @@
+package com.greedy.moaware.work.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name="WORK")
+public class WorkTime {
+	
+	@Column(name="WORK_TIME")
+	private Date workTime;
+	
+	@Column(name="QUIT_TIME")
+	private Date quitTime;
+	
+	@Column(name="WORK_STATUS")
+	private String workStatus;
+	
+	@EmbeddedId
+	private WorkPk workPk;
+
+}
