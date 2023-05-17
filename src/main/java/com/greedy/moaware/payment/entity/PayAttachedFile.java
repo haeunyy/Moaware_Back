7fe78bf56f97e7272c80a65b2367049abf4d9b5f
cloverpig <byoungoh.kang@gmail.com>
2023-05-17 17:35:43 +0900

    2023.05.17 feat payment 폴더 내용 추가중

---
+++ src/main/java/com/greedy/moaware/payment/entity/PayAttachedFile.java
@@ -0,0 +1,37 @@
+package com.greedy.moaware.payment.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name="ATTACHED_FILE")
+@SequenceGenerator(name="FILE_SEQ_GENERATOR", sequenceName="SEQ_ATTACHMENT_FILE_CODE", initialValue=1, allocationSize=1)
+public class PayAttachedFile {
+	
+	@Id
+	@Column(name="FILE_CODE")
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="FILE_SEQ_GENERATOR")
+	private Integer fileCode;
+	
+	@Column(name="ORIGINAL_FILE_NAME")
+	private String originalFileName;
+	
+	@Column(name="FILE_PATH")
+	private String filePath;
+	
+	@Column(name="SAVED_FILE_NAME")
+	private String savedFileName;
+	
+	
+
+}
---
+++ src/main/java/com/greedy/moaware/payment/entity/PayEmp.java
@@ -0,0 +1,66 @@
+package com.greedy.moaware.payment.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name="EMPLOYEE")
+@SequenceGenerator(name="EMP_SEQ_GENERATOR", sequenceName="SEQ_EMP_CODE", initialValue=1, allocationSize=1)
+public class PayEmp {
+	
+	@Id
+	@Column(name="EMP_CODE")
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="EMP_SEQ_GENERATOR")
+	private Integer empCode;
+	
+	@Column(name="EMP_NAME")
+	private String empName;
+	
+	@Column(name="PHONE")
+	private String phone;
+	
+	@Column(name="EMP_ID")
+	private String empID;
+	
+	@Column(name="EMAIL")
+	private String email;
+	
+	@Column(name="RETIRE_YN")
+	private String retireYn;
+	
+	@Column(name="HIRE_DATE")
+	private Date hireDate;
+	
+	@Column(name="EXTENSION_NUM")
+	private String extensionNum;
+	
+	@Column(name="EMP_SSI")
+	private String empSsi;
+	
+	@ManyToOne
+	@JoinColumn(name="JOB_CODE")
+	private PayJob job;
+	
+	@ManyToOne
+	@JoinColumn(name="DEPT_CODE")
+	private PayDept dept;
+	
+	
+	
+
+}
