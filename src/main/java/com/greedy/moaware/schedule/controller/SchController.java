3d13358bb4a975b8d1c73d7fd9becaa3ed1bbf65
hyojin jeon <hj90886844@gmail.com>
2023-05-30 09:41:14 +0900

    효진 / 일정관리 생성

--- src/main/java/com/greedy/moaware/schedule/controller/SchController.java
+++ src/main/java/com/greedy/moaware/schedule/controller/SchController.java
@@ -7,11 +7,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.greedy.moaware.common.ResponseDto;
 import com.greedy.moaware.employee.dto.AuthEmpDto;
+import com.greedy.moaware.employee.dto.EmpDto;
 import com.greedy.moaware.schedule.dto.ScheduleDto;
 import com.greedy.moaware.schedule.service.SchService;
 
@@ -33,28 +36,25 @@ public class SchController {
 		return "안냥 이거는 한번해보는 실험이야";
 	}
 	
-	/* 전체 캘린더 조회 - 생성자, 참여자 일정 모두 포함 */
+	/* 전체 캘린더 조회 */
 	@GetMapping("/calendar")
-	public ResponseEntity<ResponseDto> mySchList(@AuthenticationPrincipal AuthEmpDto emp) {
+	public ResponseEntity<ResponseDto> mySchList(@AuthenticationPrincipal AuthEmpDto authEmp) {
 	
 		log.info("[SchController] : mySchList start ===================================== ");
-
 		
-//		List<Schedule> schedules = schService.getScheduleListByUser(emp.getEmpCode());
-		
-		List<ScheduleDto> schedules = schService.getScheduleListByUser(emp.getEmpCode());
+		List<ScheduleDto> schedules = schService.getScheduleListByUser(authEmp.getEmpCode());
 		
 		log.info("[SchController] : mySchList  end  ===================================== ");
 		
 		return ResponseEntity
 				.ok()
-				.body(new ResponseDto(HttpStatus.OK, "전체 캘린더 조회 완료", schService.getScheduleListByUser(emp.getEmpCode())));
+				.body(new ResponseDto(HttpStatus.OK, "전체 캘린더 조회 완료", schService.getScheduleListByUser(authEmp.getEmpCode())));
 
 	}
 	
-	/* 상세 캘린더 조회 - 생성자, 참여자 일정 모두 포함 */
+	/* 상세 캘린더 조회 */
 	@GetMapping("/calendar/{schCode}")
-	public ResponseEntity<ResponseDto> mySchDetail(@AuthenticationPrincipal AuthEmpDto emp, @PathVariable("schCode") Integer schCode) {
+	public ResponseEntity<ResponseDto> mySchDetail(@AuthenticationPrincipal AuthEmpDto authEmp, @PathVariable("schCode") Integer schCode) {
 
 		log.info("[SchController] : mySchDetail start ===================================== ");
 
@@ -62,7 +62,23 @@ public class SchController {
 
 	    return ResponseEntity 
 	            .ok()
-	            .body(new ResponseDto(HttpStatus.OK, "상세 일정 조회 완료", schService.getScheduleByCodeAndUser(schCode, emp.getEmpCode())));
+	            .body(new ResponseDto(HttpStatus.OK, "상세 일정 조회 완료", schService.getScheduleByCodeAndUser(schCode, authEmp.getEmpCode())));
+	}
+	
+	/* 일정 등록 */
+	@PostMapping("/calendar")
+	public ResponseEntity<ResponseDto> insertSchedule(@AuthenticationPrincipal AuthEmpDto authEmp, @RequestBody ScheduleDto scheduleDto) {
+
+		log.info("[SchController] : insertSchedule start ===================================== ");
+
+		schService.insertSchedule(scheduleDto);
+		
+	    log.info("[SchController] : insertSchedule  end  ===================================== ");
+	    
+	    return ResponseEntity 
+	            .ok()
+	            .body(new ResponseDto(HttpStatus.OK, "일정 등록 성공"));
+	    		
 	}
 	
 }
--- src/main/java/com/greedy/moaware/schedule/service/SchService.java
+++ src/main/java/com/greedy/moaware/schedule/service/SchService.java
@@ -3,16 +3,19 @@ package com.greedy.moaware.schedule.service;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.transaction.Transactional;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
-import com.greedy.moaware.employee.entity.AuthEmp;
 import com.greedy.moaware.employee.entity.Emp;
 import com.greedy.moaware.employee.repository.EmpRepository;
 import com.greedy.moaware.exception.ScheduleNotFoundException;
 import com.greedy.moaware.exception.UserNotFoundException;
 import com.greedy.moaware.schedule.dto.ScheduleDto;
 import com.greedy.moaware.schedule.entity.Schedule;
+import com.greedy.moaware.schedule.repository.SchCategoryRepository;
 import com.greedy.moaware.schedule.repository.SchRepository;
 
 import lombok.extern.slf4j.Slf4j;
@@ -22,23 +25,26 @@ import lombok.extern.slf4j.Slf4j;
 public class SchService {
 
     private final SchRepository schRepository;
+    private final SchCategoryRepository schCategoryRepository;
     private final EmpRepository empRepository;
     private ModelMapper modelMapper;
     
-    public SchService(SchRepository schRepository, EmpRepository empRepository, ModelMapper modelMapper) {
+    public SchService(SchRepository schRepository, SchCategoryRepository schCategoryRepository, EmpRepository empRepository, ModelMapper modelMapper) {
         this.schRepository = schRepository;
+        this.schCategoryRepository = schCategoryRepository;
         this.empRepository = empRepository;
         this.modelMapper = modelMapper;
     }
 
-    public List<ScheduleDto> getScheduleListByUser(Integer emp) {
+    /* 전체 캘린더 조회 */
+    public List<ScheduleDto> getScheduleListByUser(Integer authEmp) {
     	
-        Emp employee = empRepository.findById(emp)
-                .orElseThrow(() -> new UserNotFoundException(emp + "번의 사원을 찾을 둘수 없습니다."));
+        Emp employee = empRepository.findById(authEmp)
+                .orElseThrow(() -> new UserNotFoundException(authEmp + "번의 사원을 찾을 둘수 없습니다."));
         
         List<Schedule> schedules = schRepository.findBySchAuthor(employee);
         
-        log.info("getScheduleListByUser: {}", schedules);
+        log.info("[SchService] getScheduleListByUser: {}", schedules);
         
         List<ScheduleDto> scheduleDto = schedules.stream()
                 .map(schedule -> modelMapper.map(schedule, ScheduleDto.class))
@@ -47,18 +53,39 @@ public class SchService {
         return scheduleDto;
     }
 
-	public ScheduleDto getScheduleByCodeAndUser(Integer schCode, Integer emp) {
+    /* 상세 캘린더 조회 */
+	public ScheduleDto getScheduleByCodeAndUser(Integer schCode, Integer authEmp) {
 		
-		Emp employee = empRepository.findById(emp)
-	            .orElseThrow(() -> new UserNotFoundException(emp + "번의 사원을 찾을 수 없습니다."));
+		Emp employee = empRepository.findById(authEmp)
+	            .orElseThrow(() -> new UserNotFoundException(authEmp + "번의 사원을 찾을 수 없습니다."));
 		
 		Schedule schedule = schRepository.findBySchCodeAndSchAuthor(schCode, employee)
 	            .orElseThrow(() -> new ScheduleNotFoundException(schCode + "번의 일정을 찾을 수 없습니다."));
 
-	    log.info("getScheduleByCodeAndUser: {}", schedule);
+	    log.info("[SchService] getScheduleByCodeAndUser: {}", schedule);
 
 	    return modelMapper.map(schedule, ScheduleDto.class);
 	}
 
+	/* 일정 등록 */
+	@Transactional
+	public void insertSchedule(@ModelAttribute ScheduleDto scheduleDto) {
+		
+		log.info("[SchService] insertSchedule: {}", scheduleDto);
+		
+		schRepository.save(modelMapper.map(scheduleDto, Schedule.class));		
+	
+	}
+
+	/* 일정 수정 */
+	@Transactional
+	public void modifySchedule(ScheduleDto scheduleDto) {
+		
+		log.info("[SchService] modifySchedule: {}", scheduleDto);
+		
+	}
+	
+	/* 일정 삭제 */
+	
 }
 
