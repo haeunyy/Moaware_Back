36b02f5fbd4cefe6a40cd7f4d237023f1f8542c0
cloverpig <byoungoh.kang@gmail.com>
2023-06-05 15:32:05 +0900

    2023.06.05 병오 log.info 완료

--- src/main/java/com/greedy/moaware/organization/service/OrganizationService.java
+++ src/main/java/com/greedy/moaware/organization/service/OrganizationService.java
@@ -29,44 +29,30 @@ public class OrganizationService {
 	/* 조직도 상위부서 검색 */
 	public List<OrganizationDto> selectOranizationList() {
 		
-		log.info("[OrganizationService] selectOranizationList start ============================== ");
-		
 		List<Organization> orgList = organizationRepository.findAll();
 		
 		List<OrganizationDto> orgDtoList = orgList.stream().map( org -> modelMapper.map(org, OrganizationDto.class)).collect(Collectors.toList());
-		
-		log.info("[OrganizationService] selectOranizationList start ============================== ");
-		
+
 		return orgDtoList;
 	}
 	
 	/* 조직도 하위 부서 및 직원 검색 */
 	public List<OrganizationDto> selectOranizationSubList( Integer refDeptCode) {
-		
-		log.info("[OrganizationService] selectOranizationList start ============================== ");
-		
+
 		List<Organization> orgList = organizationRepository.findByRefDeptCode(refDeptCode);
 		
 		List<OrganizationDto> orgDtoList = orgList.stream().map( org -> modelMapper.map(org, OrganizationDto.class)).collect(Collectors.toList());
-		
-		log.info("[OrganizationService] selectOranizationList start ============================== ");
-		
+
 		return orgDtoList;
 	}
 
 	/* 조직도 이름,직급,직책으로 검색 */
 	public List<SearchOrganizationDto> selectOrgSearch(String search) {
-		
-		log.info("[EmpService] selectOrgDetail start ============================== ");
-		log.info("[EmpService] search : {}" , search);
-		
+
 		List<Organization> orgList = organizationRepository.findBySearch(search);
-		log.info("[EmpService] orgList : {}" , orgList);	
-		
+
 		List<SearchOrganizationDto> orgDtoList = orgList.stream().map( org -> modelMapper.map(org, SearchOrganizationDto.class)).collect(Collectors.toList());
-		log.info("[EmpService] orgDtoList : {}" , orgDtoList);	
-		
-		log.info("[EmpService] selectOrgDetail end ================================ ");
+
 		return orgDtoList;
 	}
 
--- src/main/java/com/greedy/moaware/payment/entity/RefenceMember.java
+++ src/main/java/com/greedy/moaware/payment/entity/RefenceMember.java
@@ -10,7 +10,6 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-@ToString
 @Getter
 @Setter
 @Entity
