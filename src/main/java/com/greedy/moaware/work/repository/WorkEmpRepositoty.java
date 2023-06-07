5f11af1662fd040cce2b764bb66c91cde1f56912
dldbaud <dldbaud20@gmail.com>
2023-06-07 14:40:30 +0900

    네이티브 쿼리 MySql 구문으로 변경

--- src/main/java/com/greedy/moaware/work/repository/WorkEmpRepositoty.java
+++ src/main/java/com/greedy/moaware/work/repository/WorkEmpRepositoty.java
@@ -13,12 +13,10 @@ import com.greedy.moaware.work.entity.WorkPk;
 
 public interface WorkEmpRepositoty extends JpaRepository<WorkEmp2, WorkPk>{
 
-    @Query(value = "SELECT * FROM EMPLOYEE e LEFT JOIN WORK w ON e.EMP_CODE = w.EMP_CODE AND w.WORK_DATE = :workDate",
-            nativeQuery = true)
+    @Query(value = "SELECT * FROM EMPLOYEE e LEFT JOIN WORK w ON e.EMP_CODE = w.EMP_CODE AND DATE(w.WORK_DATE) = :workDate", nativeQuery = true)
 	Page<WorkEmp2> findByWorkWorkPkWorkDate(@Param("workDate") Date workDate, Pageable pageable);
     
-    @Query(value = "SELECT * FROM EMPLOYEE e LEFT JOIN WORK w ON e.EMP_CODE = w.EMP_CODE AND w.WORK_DATE = :workDate WHERE e.EMP_NAME LIKE %:empName% AND e.RETIRE_YN = 'N' ORDER BY w.WORK_TIME DESC",
-            nativeQuery = true)
+    @Query(value = "SELECT * FROM EMPLOYEE e LEFT JOIN WORK w ON e.EMP_CODE = w.EMP_CODE AND w.WORK_DATE = :workDate WHERE e.EMP_NAME LIKE CONCAT('%', :empName, '%') AND e.RETIRE_YN = 'N' ORDER BY w.WORK_TIME DESC", nativeQuery = true)
     Page<WorkEmp2> findByEmpNameAndWorkWorkPkWorkDate(@Param("empName") String name, Date workDate, Pageable pageable);
 
 
